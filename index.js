82e7bad6f18160ab4fb9c3f7c26ce68d42e6229d
fadimoayad <fadimoayad99@yahoo.com>
2022-12-03 23:29:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+let newHeader=document.createElement('h1');
+newHeader.setAttribute('id','victory')
+newHeader.textContent="is the champion";
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+const element = document.createElement("div");
+document.body.appendChild(element);
+
+const ul=document.createElement('ul');
+
+for(let i=0;i<3;i++){
+    const li=document.createElement('li');
+    li.textContent=(i+1).toString();
+    ul.appendChild(li);
+}
+element.appendChild(ul)
